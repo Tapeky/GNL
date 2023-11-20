0c41a7b31cae245e5b9490cf037a4aa931e99741
Tareq Sadouk <tsadouk@2A7.42angouleme.fr>
2023-11-20 13:05:19 +0100

    Brice

--- get_next_line.c
+++ get_next_line.c
@@ -3,11 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tsadouk <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: tsadouk <tsadouk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 10:36:51 by tsadouk           #+#    #+#             */
-/*   Updated: 2023/11/20 10:36:57 by tsadouk          ###   ########.fr       */
+/*   Updated: 2023/11/20 11:31:57 by tsadouk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+// lire jusquw \n
+// stocker dans static
+// char temp = debut - first \n
+// fonction qui modifie la static = la static sans la pemiere ligne 
+// strjoin entre static et la premiere ligne
 
+char *get_next_line(int fd)
+{
+
+	
+}
--- get_next_line.h
+++ get_next_line.h
@@ -3,11 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tsadouk <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: tsadouk <tsadouk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 10:37:05 by tsadouk           #+#    #+#             */
-/*   Updated: 2023/11/20 10:37:07 by tsadouk          ###   ########.fr       */
+/*   Updated: 2023/11/20 11:25:26 by tsadouk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
 
+char	*get_next_line(int fd);
+
+
+#endif
